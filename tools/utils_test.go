543f05465eab3dd19463caf7371b529fccac1355
dtrckd <dtrckd@gmail.com>
2022-10-18 15:39:09 +0200

    test: add utils_test.go

---
+++ tools/utils_test.go
@@ -0,0 +1,35 @@
+package tools
+
+import (
+	"fractale/fractal6.go/graph/model"
+	"reflect"
+	"testing"
+)
+
+func TestStructMap(t *testing.T) {
+
+    var nodeFragment *model.NodeFragment
+    var nodeInput model.AddNodeInput
+
+    name := "name"
+    nameid := "nameid"
+    username := "username"
+    nodeFragment = &model.NodeFragment{
+        Name: &name,
+        Nameid: &nameid,
+        FirstLink: &username,
+    }
+
+    StructMap(nodeFragment, &nodeInput)
+
+    // FirstLink cannot be added by adding a node !
+    want := model.AddNodeInput{
+        Name: name,
+        Nameid: nameid,
+    }
+
+    if reflect.DeepEqual(nodeInput, want) {
+        t.Errorf("StructMap error, want: %v, got: %v", want, nodeInput)
+    }
+}
+
